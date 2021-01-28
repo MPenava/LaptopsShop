69131658ddabd33aa3cbe5478bf50b1cc78cbf20
MPenava <marko.penava@fpmoz.sum.ba>
2021-01-28 09:41:53 +0100

    We change DB name

--- model/db.php
+++ model/db.php
@@ -1,5 +1,5 @@
 <?php
-    $conn=mysqli_connect('studenti.sum.ba','fpmoz152021','csdigital2021','fpmoz152021');
+    $conn=mysqli_connect('fpmoz152021','fpmoz152021','csdigital2021');
 
     if(!$conn){
         die ("Conection failed!" . mysqli_connect_error());
