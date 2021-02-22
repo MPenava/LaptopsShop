513b27c7e9bbd5765f445376d235f559fa737f6a
unknown <marko.penava@fpmoz.sum.ba>
2021-02-22 10:29:07 +0100

    --"

--- public/index.php
+++ public/index.php
@@ -32,7 +32,7 @@
     <link rel="shortcut icon" href='assets/logo/ls-icon.png'>
     <title>Laptops Shop</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://kit.fontawesome.com/47f0b88d10.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="css/stylee.css">
