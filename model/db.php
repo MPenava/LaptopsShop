1062ed27d3de3482ee7da0b538302f40780d14b3
unknown <marko.penava@fpmoz.sum.ba>
2021-03-15 12:23:35 +0100

    Some changes

--- model/db.php
+++ model/db.php
@@ -1,6 +1,6 @@
 <?php
-    // $conn=mysqli_connect('localhost','fpmoz152021','csdigital2021','fpmoz152021');
-    $conn=mysqli_connect('localhost','root','','ls_shop');
+    $conn=mysqli_connect('localhost','fpmoz152021','csdigital2021','fpmoz152021');
+    //$conn=mysqli_connect('localhost','root','','ls_shop');
 
     if(!$conn){
         die ("Conection failed!" . mysqli_connect_error());
